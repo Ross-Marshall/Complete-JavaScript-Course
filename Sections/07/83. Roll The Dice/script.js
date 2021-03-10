186367fc6a4cd67e188e1938fac5bbd6d62e510b
Ross-Marshall <rossmarshallgithub@gmail.com>
2021-03-10 08:05:48 -0500

    Add files via upload

---
+++ Sections/07/83. Roll The Dice/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0'); // Use Class - faster
+const score1El = document.getElementById('score--1'); // Use ID
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Roll dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1:  if true, switch to next player
+});
