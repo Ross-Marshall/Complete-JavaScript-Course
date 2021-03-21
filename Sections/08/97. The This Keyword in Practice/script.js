ff8d2690361e4c71a54239ddd278f69a9a170246
Ross-Marshall <rossmarshallgithub@gmail.com>
2021-03-21 19:25:20 -0400

    Add files via upload

---
+++ Sections/08/97. The This Keyword in Practice/script.js
@@ -0,0 +1,87 @@
+'use strict';
+
+/*
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+console.log(addArrow);
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(y === window.z);
+
+console.log(window);
+
+const jonas = {
+  name: 'Jonas',
+  year: 1989,
+  calcAge: function () {
+    return 2037 - this.year;
+  },
+};
+
+console.log(jonas.calcAge());
+
+*/
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+
+matilda.calcAge();
+
+console.log(matilda);
+
+const f = jonas.calcAge;
+
+f();
