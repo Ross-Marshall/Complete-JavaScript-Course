aae21c2ad36a44227ff671d372369f71cb9df98b
Ross-Marshall <rossmarshallgithub@gmail.com>
2021-03-20 21:02:22 -0400

    Add files via upload

---
+++ Sections/08/95. Hoisting and TDZ in Practice/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+console.log(addArrow);
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(y === window.z);
+
+console.log(window);
