e65d155f4a0eab37eab0fe19d0a84126a49a7e80
Ross-Marshall <rossmarshallgithub@gmail.com>
2021-03-22 21:26:51 -0400

    Add files via upload

---
+++ Sections/08/98. Regular Functions vs. Arrow Functions/script.js
@@ -0,0 +1,137 @@
+'use strict';
+
+/*
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+console.log(addArrow);
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(y === window.z);
+
+console.log(window);
+
+const jonas = {
+  name: 'Jonas',
+  year: 1989,
+  calcAge: function () {
+    return 2037 - this.year;
+  },
+};
+
+console.log(jonas.calcAge());
+
+
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+
+matilda.calcAge();
+
+console.log(matilda);
+
+const f = jonas.calcAge;
+
+f();
+
+*/
+
+let firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // self = this;
+    // const isMilenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMilenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMilenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+jonas.greet();
+console.log(this.firstName);
+
+jonas.calcAge();
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 3, 4, 5, 6);
