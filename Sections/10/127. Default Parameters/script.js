1de37596ae07fe73d35254a890630781884f474b
Ross-Marshall <rossmarshallgithub@gmail.com>
2021-04-29 19:55:29 -0400

    Add files via upload

---
+++ Sections/10/127. Default Parameters/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // Pre - ES6
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LM123');
+createBooking('LM123', 2, 800);
+createBooking('LM123', 2);
+createBooking('LM123', 5);
+
+createBooking('LM123', undefined, 1000);
+createBooking('LM123', 0, 1000);
+createBooking('LM123', false, 1000);
