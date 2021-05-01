7887067f06cbe1f944cece652de2b9ac330fba5a
Ross-Marshall <rossmarshallgithub@gmail.com>
2021-05-01 19:41:39 -0400

    Add files via upload

---
+++ Sections/10/129. First-Class and High-Order Functions/script.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // Pre - ES6
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LM123');
+createBooking('LM123', 2, 800);
+createBooking('LM123', 2);
+createBooking('LM123', 5);
+
+createBooking('LM123', undefined, 1000);
+createBooking('LM123', 0, 1000);
+createBooking('LM123', false, 1000);
+
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 23245535353,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23245535353) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // flightNum = flight;
+// const passenger = jonas;
+// const flightNum = flight;
+
+//console.log(flight, passenger);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 77728838832);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
+
+/*
+
+First Class Function - 
+
+Higher Order Function - Function that receives another function.
+
+*/
